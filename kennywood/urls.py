066c1c619b6eea8edcf3b8c0205d10823da4788a
Ryan Bishop <rdbishop06@gmail.com>
2020-02-17 16:00:22 -0600

    setup login and register url routes with auth

--- kennywood/urls.py
+++ kennywood/urls.py
@@ -17,13 +17,15 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from kennywoodapi.models import *
-from kennywoodapi.views import ParkAreas
+from kennywoodapi.views import ParkAreas, register_user, login_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'parkareas', ParkAreas, 'parkarea')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', register_user),
+    path('login/', login_user),
     path('api-token-auth/', obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
